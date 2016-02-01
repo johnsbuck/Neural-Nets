8d204d2b3a2bd4eeefb5952fe54f6261c64dfe15
CornyElephant <eldraco@mac.com>
2016-02-01 02:48:32 -0500

    added some comments

--- jpegConvert.py
+++ jpegConvert.py
@@ -5,15 +5,16 @@ import ForwardNN
 import os.path
 from PIL import Image
 
+#simple script to get used to python, reads image files (with digit they represent as first char in name) and creates training set files for script.py
 def visual(argv):
-
+    #iterate through all image files in training set
     for fn in os.listdir(os.getcwd() + '/TrainingSet/'):
-        #print("what is up")
         print(fn)
         image = Image.open("TrainingSet/" + fn)
         inputs = argv.inputs[0]
         image.show()
         pix = list(image.getdata())
+        #iterate through pixels, if red channel isn't 255 (read: if pixel isn't perfectly white), then will mark as filled in
         first = True
         for pixel in pix:
             if first:
@@ -26,6 +27,7 @@ def visual(argv):
                 inputs.write("0")
         inputs.write("\n")
 
+        #file names must start with the digit they represent. Totally a bad hack, this script is just to test performance and will be deprecated
         number = int(fn[0])
 
         for x in range(1,number):
